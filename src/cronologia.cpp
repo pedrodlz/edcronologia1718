ce470cc9e0b325dd4243adbacfb04a29771c5a7d
pedrodlz <33036078+pedrodlz@users.noreply.github.com>
2017-10-23 21:31:15 +0200

    Update cronologia.cpp

--- src/cronologia.cpp
+++ src/cronologia.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include "cronologia.h"
 
+using namespace std;
+
+Cronologia::Cronologia(void){}
+
+Cronologia::Cronologia(){}
+
+Cronologia::Cronologia(const Cronologia & otra)
+{
+	fechas = otra.fechas;
+}
+
+void Cronologia::AniadeFecha(FechaHistorica una_fecha)
+{
+  int anio = una_fecha.GetAnio();
+  bool no_encontrado = true;
+  
+	for(int i = o; i<MAX_ANIO && no_encontrado; i++){
+    if(i == anio){
+      no_encontrado = false;
+      
+  }
+}
+
+void Cronologia::EliminaFecha(int anio)
+{
+	int p;
+	
+	for(p = i; p < sucesos.size() - 1; p++)
+		sucesos[p] = sucesos[p + 1];
+	
+	sucesos.resize(p);
+}
+
+void Cronologia::UnionCrono(Cronologia cron1,Cronologia cron2)
+{
+  
+}
