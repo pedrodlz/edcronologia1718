75516cef6606537d082f52fb956bbddcbf36b1ac
pedrodlz <33036078+pedrodlz@users.noreply.github.com>
2017-10-23 21:15:08 +0200

    Update cronologia.h

--- include/cronologia.h
+++ include/cronologia.h
@@ -1,1 +1,39 @@
+#ifndef CRONOLOGIA
+#define CRONOLOGIA
 
+#include <string>
+#include "Vector_Dinamico.h"
+#include "fechahistorica.h"
+
+using namespace std;
+
+
+class Cronologia{
+
+private:
+	
+	Vector_Dinamico<FechaHistorica> anios;
+
+public:
+  
+	Cronologia(void);
+  
+  Cronologia();
+
+	Cronologia(const Cronologia & otra);	
+
+	void AniadeFecha(FechaHistorica una_fecha);
+	
+	void EliminaFecha(int anio);
+  
+  void UnionCrono(Cronologia cron1, Cronologia cron2);
+
+
+private:
+	void LiberarMemoria (void);
+
+
+};
+
+
+#endif
