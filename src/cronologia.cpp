0ef39ebe6b769b068dacb127398d9bb1a386363a
Pedro <33036078+pedrodlz@users.noreply.github.com>
2017-10-25 10:43:42 +0200

    Terminado AniadeFecha y editado EliminaFecha
    en EliminaFecha se ha añadido this.

--- src/cronologia.cpp
+++ src/cronologia.cpp
@@ -9,17 +9,25 @@ Cronologia::Cronologia(void){}
 
 Cronologia::Cronologia(const Cronologia & otra)
 {
-	fechas = otra.fechas;
+	this.fechas = otra.fechas;
 }
 
 void Cronologia::AniadeFecha(FechaHistorica una_fecha)
 {
 	int anio = una_fecha.GetAnio();
 	bool no_encontrado = true;
-
-	for(int i = 0; i<MAX_ANIO && no_encontrado; i++){
-		if(i == anio){
-		  no_encontrado = false;
+	
+	for(int i = 0; i < this.fechas.size() && no_encontrado; i++){
+		if(this.fechas[i].GetAnio == anio){
+			for(int p = 0; p < this.fechas[i].sucesos.size(); p ++)
+				this.fechas[i].AniadeSuceso(this.fechas[i].sucesos[p]);
+			no_encontrado = false;
+		}
+		else if(this.fechas[i].GetAnio < anio && this.fechas[i+1].GetAnio > anio){
+			this.fechas.resize(fechas.size() + 1);
+			for(int p = this.fechas.size(); p > i-1; p--)
+				this.fechas[p] = this.echas[p-1];
+			this.fechas[i+1] = una_fecha;
 		}
 	}
 }
@@ -29,14 +37,14 @@ void Cronologia::EliminaFecha(int anio)
 	bool no_encontrado = true;
 	int p;
 	
-	for(int i = 0; i < fechas.size() && no_encontrado; i++)
-		if(fechas[i].GetAnio == anio){
-			for(p = i; p < fechas.size() - 1; p++)
-				fechas[p] = fechas[p+1];
+	for(int i = 0; i < this.fechas.size() && no_encontrado; i++)
+		if(this.fechas[i].GetAnio == anio){
+			for(p = i; p < this.fechas.size() - 1; p++)
+				this.fechas[p] = this.fechas[p+1];
 			no_encontrado = false;
 		}
 	
-	fechas.resize(p);	
+	this.fechas.resize(p);	
 }
 
 void Cronologia::UnionCrono(Cronologia cron1,Cronologia cron2)
