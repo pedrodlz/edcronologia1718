e3ab5bd21643825dc0ea9379b16a0b2586cde035
Pedro <33036078+pedrodlz@users.noreply.github.com>
2017-10-25 11:20:15 +0200

    Añadidos filtros para añadir y eliminar fechas

--- src/cronologia.cpp
+++ src/cronologia.cpp
@@ -15,36 +15,43 @@ Cronologia::Cronologia(const Cronologia & otra)
 void Cronologia::AniadeFecha(FechaHistorica una_fecha)
 {
 	int anio = una_fecha.GetAnio();
-	bool no_encontrado = true;
 	
-	for(int i = 0; i < this.fechas.size() && no_encontrado; i++){
-		if(this.fechas[i].GetAnio == anio){
-			for(int p = 0; p < this.fechas[i].sucesos.size(); p ++)
-				this.fechas[i].AniadeSuceso(this.fechas[i].sucesos[p]);
-			no_encontrado = false;
-		}
-		else if(this.fechas[i].GetAnio < anio && this.fechas[i+1].GetAnio > anio){
-			this.fechas.resize(fechas.size() + 1);
-			for(int p = this.fechas.size(); p > i-1; p--)
-				this.fechas[p] = this.echas[p-1];
-			this.fechas[i+1] = una_fecha;
+	if(anio >= MIN_ANIO && anio <= MAX_ANIO){
+		
+		bool no_encontrado = true;
+		
+		for(int i = 0; i < this.fechas.size() && no_encontrado; i++){
+			if(this.fechas[i].GetAnio == anio){
+				for(int p = 0; p < this.fechas[i].sucesos.size(); p ++)
+					this.fechas[i].AniadeSuceso(this.fechas[i].sucesos[p]);
+				no_encontrado = false;
+			}
+			else if(this.fechas[i].GetAnio < anio && this.fechas[i+1].GetAnio > anio){
+				this.fechas.resize(fechas.size() + 1);
+				for(int p = this.fechas.size(); p > i-1; p--)
+					this.fechas[p] = this.echas[p-1];
+				this.fechas[i+1] = una_fecha;
+			}
 		}
 	}
 }
 
 void Cronologia::EliminaFecha(int anio)
 {
-	bool no_encontrado = true;
-	int p;
+	if(anio >= MIN_ANIO && anio <= MAX_ANIO){
 	
-	for(int i = 0; i < this.fechas.size() && no_encontrado; i++)
-		if(this.fechas[i].GetAnio == anio){
-			for(p = i; p < this.fechas.size() - 1; p++)
-				this.fechas[p] = this.fechas[p+1];
-			no_encontrado = false;
-		}
-	
-	this.fechas.resize(p);	
+		bool no_encontrado = true;
+		int p;
+
+		for(int i = 0; i < this.fechas.size() && no_encontrado; i++)
+			if(this.fechas[i].GetAnio == anio){
+				for(p = i; p < this.fechas.size() - 1; p++)
+					this.fechas[p] = this.fechas[p+1];
+				no_encontrado = false;
+			}
+
+		this.fechas.resize(p);	
+	}
 }
 
 void Cronologia::UnionCrono(Cronologia cron_aniadir)
