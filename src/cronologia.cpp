35c427803db3a36ec23c67544dfa137cd360f47f
Pedro <33036078+pedrodlz@users.noreply.github.com>
2017-10-25 10:24:51 +0200

    Terminado EliminaFecha (o eso creo)

--- src/cronologia.cpp
+++ src/cronologia.cpp
@@ -14,25 +14,29 @@ Cronologia::Cronologia(const Cronologia & otra)
 
 void Cronologia::AniadeFecha(FechaHistorica una_fecha)
 {
-  int anio = una_fecha.GetAnio();
-  bool no_encontrado = true;
+	int anio = una_fecha.GetAnio();
+	bool no_encontrado = true;
 
 	for(int i = 0; i<MAX_ANIO && no_encontrado; i++){
-	    if(i == anio){
-	      no_encontrado = false;
+		if(i == anio){
+		  no_encontrado = false;
 		}
 	}
 }
 
 void Cronologia::EliminaFecha(int anio)
 {
-/*	int p;
-
-	for(p = i; p < sucesos.size() - 1; p++)
-		sucesos[p] = sucesos[p + 1];
-
-	sucesos.resize(p);
-	*/
+	bool no_encontrado = true;
+	int p;
+	
+	for(int i = 0; i < fechas.size() && no_encontrado; i++)
+		if(fechas[i].GetAnio == anio){
+			for(p = i; p < fechas.size() - 1; p++)
+				fechas[p] = fechas[p+1];
+			no_encontrado = false;
+		}
+	
+	fechas.resize(p);	
 }
 
 void Cronologia::UnionCrono(Cronologia cron1,Cronologia cron2)
